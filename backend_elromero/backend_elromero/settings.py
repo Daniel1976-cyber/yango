3fb5082093b54d51a3d07af5ac3eb43fce7aaf2a
Daniel1976-cyber <aybcaneyes@gmail.com>
2026-02-18 04:22:27 +0100

    dokerizando

--- backend_elromero/backend_elromero/settings.py
+++ backend_elromero/backend_elromero/settings.py
@@ -78,13 +78,23 @@ WSGI_APPLICATION = 'backend_elromero.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
+import os
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if os.environ.get('DATABASE_URL'):
+    import dj_database_url
+    DATABASES = {
+        'default': dj_database_url.config(
+            default='sqlite:///db.sqlite3',
+            conn_max_age=600
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
