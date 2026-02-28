96dce216820ce9e398cbf6fa963fc31aa7c5488d
Daniel1976-cyber <aybcaneyes@gmail.com>
2026-02-28 03:08:35 +0100

    entrando con vercel

--- backend_elromero/backend_elromero/wsgi.py
+++ backend_elromero/backend_elromero/wsgi.py
@@ -10,8 +10,19 @@ https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
 import os
 
 from django.core.wsgi import get_wsgi_application
+from django.core.management import call_command
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'backend_elromero.settings')
 
 application = get_wsgi_application()
+
+# Automatic migrations on startup
+try:
+    print("Running automatic migrations...")
+    call_command('migrate', interactive=False)
+    print("Migrations completed successfully.")
+except Exception as e:
+    print(f"Error running migrations: {e}")
+
+# Add for Vercel
 app = application
