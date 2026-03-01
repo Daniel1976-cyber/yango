68380da1880ae97a3011c94b965bd8b6c3a33c8e
Daniel1976-cyber <aybcaneyes@gmail.com>
2026-03-01 12:43:25 +0100

    hfd

--- backend_elromero/tienda/tienda_urls.py
+++ backend_elromero/tienda/tienda_urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from . import tienda_views as views
+from .supabase_api import productos_api, agregar_producto_api
 
 router = DefaultRouter()
 router.register(r'categorias', views.CategoriaViewSet)
@@ -10,4 +11,6 @@ router.register(r'pedidos', views.PedidoViewSet)
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('supabase/productos/', productos_api, name='supabase_productos'),
+    path('supabase/productos/agregar/', agregar_producto_api, name='supabase_agregar_producto'),
 ]
