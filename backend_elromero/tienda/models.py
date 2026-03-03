35b10dd4ef12752a8c7a4b76bc68833a9ef6a5e7
Daniel1976-cyber <aybcaneyes@gmail.com>
2026-03-03 02:38:22 +0100

    kjh

--- backend_elromero/tienda/models.py
+++ backend_elromero/tienda/models.py
@@ -26,7 +26,7 @@ class Producto(models.Model):
         return self.nombre
 
     class Meta:
-        db_table = 'productos'
+        db_table = 'producto'
         verbose_name = 'Producto'
         verbose_name_plural = 'Productos'
 
